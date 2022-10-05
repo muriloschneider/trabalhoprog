7007a75fada91c846a1bb6e591d120c4f9baee77
João Guilherme <joaoguiefernandes@gmail.com>
2022-10-04 21:57:57 -0300

    Ajax att

--- At06 Prog/js/ajax.js
+++ At06 Prog/js/ajax.js
@@ -4,7 +4,7 @@ $('#form-cidade').submit(function(e){
     var cidade = $('#cidade').val();
     var estado = $('#estado').val();
     var acao = 'Cadastrar';
-
+    if(cidade != '' && estado != ''){
     $.ajax({
         url: '../acao/acao.php',
         method: 'POST',
@@ -16,13 +16,13 @@ $('#form-cidade').submit(function(e){
         $('#estado').val('');
         getCidade();
     });
-});
+}});
 
 function getCidade(){
     $.ajax({
         url: '../acao/select.php',
         method: 'GET',
-        dataType: `JSON`    
+        dataType: `JSON`
     }).done(function(result){
         var resultado = document.querySelector('#resultado');
                 while(resultado.firstChild){
@@ -31,9 +31,7 @@ function getCidade(){
         for (var i = 0; i < result.length; i++) {
             $('#resultado').prepend('<tr><td>'+result[i].idcidade+'</td><td>'+result[i].nome_cidade+'</td><td>'+result[i].estado+'</td><td><a href="index.php?acao=Editar&id='+result[i].idcidade+'"><img src="../img/pencil-square.svg"></a></td><td><a href="../acao/acao.php?acao=Excluir&id='+result[i].idcidade+'"><img src="../img/trash-fill.svg"></a></td></tr>');
         }
-        // console.log(result);
-        // console.log('Chegou aqui');
     });
 }
-
+preventDefault();
 getCidade();
